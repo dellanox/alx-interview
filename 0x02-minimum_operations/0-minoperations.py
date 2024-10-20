ad8e9a22a3ef8455f4ffe52f0b63119a8b348aac
dellanox <code.emerem@gmail.com>
2024-10-20 05:15:32 +0100

    optimized

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
 """
-Module for Minimum Operations
+Minimum operations
 """
+
 
 def minOperations(n):
     """
-    execute only two operations in this file: Copy All and Paste
-    
-    Args
-        n (int): number of paste character
 
-    Return
-        0 if n character is not archievable, the number of copy and paste
-        operation if successful
+    :param n:
+    :return:
     """
     if n <= 1:
         return 0
-
-    count = 0
-    paste = "H"
-
-    while len(paste) < n:
-        copy = paste
-        count += 1
-        for i in range(2):
-            if len(paste) >= n:
-                break
-            count += 1
-            paste += copy
-
-    if len(paste) == n:
-        return count
-    elif len(paste) > n:
-        return count - 1
-    else:
-        return 0
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
