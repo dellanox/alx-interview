b05aa6688379f06450d8e65f4b0632bf767c0f7e
dellanox <ozioma.emerem@gmail.com>
2024-10-22 22:11:42 +0100

    error handling

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,17 +16,21 @@ def main():
             ip, _, date, time, method, directory, protocol, s_code, f_size, \
                 *r = parse
 
+            try:
+                file_size += int(f_size)
+            except ValueError:
+                pass
+
             if len(r) > 0 or len(s_code) < 3 or not s_code:
-                try:
-                    int(s_code)
-                except ValueError:
-                    continue
+                continue
+            try:
+                int(s_code)
+            except ValueError:
                 continue
             if not var.get(s_code, None):
                 var[s_code] = 1
             else:
                 var[s_code] += 1
-            file_size += int(f_size)
 
             if (i + 1) % 10 == 0:
                 print(f"File size: {file_size}")
