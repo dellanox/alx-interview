c74e3ea2746e213049f52388ba989d6aa3ea4292
dellanox <ozioma.emerem@gmail.com>
2024-10-22 21:17:27 +0100

    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,7 +13,8 @@ def main():
     try:
         for i, line in enumerate(sys.stdin):
             *parse, = line.split(" ")
-            ip, _, date, time, method, directory, protocol, s_code, f_size, *r = parse
+            ip, _, date, time, method, directory, protocol, s_code, f_size, \
+                *r = parse
 
             if len(r) > 0 or len(s_code) < 3 or not s_code:
                 try:
@@ -26,7 +27,7 @@ def main():
             else:
                 var[s_code] += 1
             file_size += int(f_size)
-            
+
             if (i + 1) % 10 == 0:
                 print(f"File size: {file_size}")
                 for x in sorted(var):
